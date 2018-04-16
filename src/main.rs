b58db3a589e093df49e13f3ef3e534bb159352c9
Marília MJ <mmjanizelli@gmail.com>
2018-04-16 14:49:35 -0300

    Guessing number program

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+include std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please input your guess: ");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+      .expect("Failed to read user's input");
+
+    println!("You guessed {}", guess);  
+
 }
